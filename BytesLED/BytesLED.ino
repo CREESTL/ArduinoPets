48d8a111e7e082766ec777dbebc4218feadb1a3a
CREESTL <creestl.main@yandex.ru>
2025-02-09 14:53:22 +0300

    Add pause between lights.

--- BytesLED/BytesLED.ino
+++ BytesLED/BytesLED.ino
@@ -2,11 +2,18 @@
   Read digits sent to Arduino from PC and turn on matching LEDs.
 
   Total of 4 LEDs on pins 2-5.
+
+  Example of input: 44421
+  Result:
+  - LED#4 turns on 3 times for 1 second
+  - LED#2 turns on for 1 second
+  - LED#1 turns on for 1 second
   Digit 1 in COM port turns on LED#1 on pin 2 for 1 second.
   Same for other digits.
 */
 
 const int dTime = 1000;
+const int switchTime = 200;
 const short minPin = 2;
 const short maxPin = 5;
 short pin;
@@ -45,6 +52,7 @@ void loop() {
       digitalWrite(pin, LOW);
       delay(dTime);
       digitalWrite(pin, HIGH);
+      delay(switchTime);
     }
   }
 }
